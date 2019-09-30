a6b3c47edcea5eaba019da02ce19b19c2f547bc2
marsbtyne <mars.ballantyne@gmail.com>
2019-09-30 14:34:30 -0400

    Updates

---
+++ precache-manifest.644e637604f5c404fc4c3a0c3affe3ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55636557f11f4b34181cc0f8b7f7fd3b",
+    "url": "/macncheese/index.html"
+  },
+  {
+    "revision": "4dce9f65d4e381b32fc6",
+    "url": "/macncheese/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8255938af32fde81dd12",
+    "url": "/macncheese/static/js/2.b1bd91ab.chunk.js"
+  },
+  {
+    "revision": "4dce9f65d4e381b32fc6",
+    "url": "/macncheese/static/js/main.c367a4d0.chunk.js"
+  },
+  {
+    "revision": "e2ae44ee2fb6e8656dcc",
+    "url": "/macncheese/static/js/runtime-main.46cbe1f3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/macncheese/static/media/logo.25bf045c.svg"
+  }
+]);
